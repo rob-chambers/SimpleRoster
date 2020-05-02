4058bc9a0712a13a24eab602978ff617fd612039
Robert Chambers <rechambers11@gmail.com>
2020-05-02 11:31:04 +0100

    Check in changes from ages ago!

--- SimpleRoster.WebClient/ClientApp/src/app/app.module.ts
+++ SimpleRoster.WebClient/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { AdminSettingsComponent } from './admin-settings/admin-settings.component';
 import { NewRosterComponent } from './new-roster/new-roster.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { NewRosterComponent } from './new-roster/new-roster.component';
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- SimpleRoster.WebClient/ClientApp/src/app/new-roster/new-roster.component.ts
+++ SimpleRoster.WebClient/ClientApp/src/app/new-roster/new-roster.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { datepicker } from '@angular/ma'
 
 @Component({
   selector: 'app-new-roster',
